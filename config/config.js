43c66506fb73e5160d7c3c284009ebe32bb49ef9
Electrifier <samchijioke16@gmail.com>
2025-02-25 14:42:26 +0100

    feat: Add payment entity to `AppDataSource`

--- config/config.js
+++ config/config.js
@@ -4,19 +4,20 @@ import { Course } from "../entities/course.entity.js"
 import { UserCourse } from "../entities/userCourse.entity.js"
 import  UserEntity  from "../entities/user.entity.js";
 import Job  from "../entities/job.entity.js";
+import { Payment } from "../entities/payment.entity.js";
 
 dotenv.config()
 
 const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: Number.parseInt(process.env.DB_PORT),
+  port: Number.parseInt(process.env.DB_PORT || '5432'),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: [UserEntity, Course, UserCourse, Job],
+  entities: [Payment, UserEntity, Course, UserCourse, Job],
   subscribers: [],
   migrations: [],
 })
